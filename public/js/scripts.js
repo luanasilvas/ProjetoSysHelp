a22082c5b426d4ff68c1a80a059ea1ff038593f2
manuel2511 <tekmanuel@gmail.com>
2024-06-17 16:20:56 -0300

    Ajustado o erro de referencia das paginas de cadastro e de edição

--- public/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const dataFinalizadoField = document.getElementById('dataFinalizado');
-    if (!dataFinalizadoField.value) {
-      const today = new Date().toISOString().split('T')[0];
-      dataFinalizadoField.value = today;
-    }
-  });
